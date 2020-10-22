27ad94440e5689d6d106122ed3fa7af52ebac7a2
Weegeesaurus <>
2020-10-21 17:39:59 -0700

    Imported Project

---
+++ HorrorJam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float sensitivity = 200f;
+    public float clampAngle = 85f;
+
+    private float verticalRotation;
+    private float horizontalRotation;
+    private void Start()
+    {
+        verticalRotation = transform.localEulerAngles.x;
+        horizontalRotation = player.transform.eulerAngles.y;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            toggleMouse();
+        }
+
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            Look();
+        }
+    }
+
+    private void Look()
+    {
+        float _mouseVertical = -Input.GetAxis("Mouse Y");
+        float _mouseHorizontal = Input.GetAxis("Mouse X");
+
+        verticalRotation += _mouseVertical * sensitivity * Time.deltaTime;
+        horizontalRotation += _mouseHorizontal * sensitivity * Time.deltaTime;
+
+        verticalRotation = Mathf.Clamp(verticalRotation, -clampAngle, clampAngle);
+
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        player.transform.rotation = Quaternion.Euler(0f, horizontalRotation, 0f);
+    }
+    public void toggleMouse()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
---
+++ HorrorJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float jumpSpeed = 5;
+    public float gravity = 9.81f;
+    public bool lockMovement;
+
+    private CharacterController controller;
+    private float yVelocity = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        gravity *= Time.fixedDeltaTime * Time.fixedDeltaTime;
+        moveSpeed *= Time.fixedDeltaTime;
+        jumpSpeed *= Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!lockMovement)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 _moveDirection = transform.right * moveHorizontal + transform.forward * moveVertical;
+            _moveDirection *= moveSpeed;
+
+            if (controller.isGrounded)
+            {
+                yVelocity = 0;
+                if (Input.GetButton("Jump"))
+                {
+                    yVelocity = jumpSpeed;
+                }
+
+            }
+            yVelocity -= gravity;
+
+            _moveDirection.y = yVelocity;
+            controller.Move(_moveDirection);
+        }
+    }
+}
