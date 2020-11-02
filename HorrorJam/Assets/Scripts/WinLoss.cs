7d3a44a68d03d18f6a0cf1165e388bf8868003c1
Nolano-Smurphy <nolan@AZMurphys.com>
2020-11-01 22:52:01 -0700

    Scenes for Game Title, Game Over, Game Complete, Game Info, and scripts for navigating the buttons/scenes as well as checking for win/loss conditions. Includes used assets.

---
+++ HorrorJam/Assets/Scripts/WinLoss.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLoss : MonoBehaviour
+{
+    public static bool gameWin;
+    public static bool gameLose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameWin = false;
+        gameLose = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameWin)
+        {
+            SceneManager.LoadScene("GameComplete");
+        }
+        if (gameLose)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
