c232efea7472ed9ce7c41e2048518caa9558db1d
Weegeesaurus <>
2020-11-05 02:26:59 -0700

    Created Forester System
    still needs a few updates

---
+++ HorrorJam/Assets/Scripts/Forester/ForesterManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForesterManager : MonoBehaviour
+{
+    public GameObject forester;
+    public LaneManager[] lanes;
+    public int laneSize;
+    public int activeLane;
+    public int currentPos;
+    public bool inside;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeLane = Random.Range(0, lanes.Length);
+        currentPos = 0;
+        Transform newPos = lanes[activeLane].NextPos(currentPos);
+        forester.transform.position = newPos.position;
+        forester.transform.rotation = newPos.rotation;
+        inside = false;
+        Invoke("MoveForester", 10f);
+    }
+
+    // Update is called once per frame
+    void MoveForester()
+    {
+        if (currentPos<laneSize-2)
+        {
+            MoveForward();
+            Invoke("MoveForester", 10f);
+        }
+        else
+        {
+            if (lanes[activeLane].IsClosed())
+            {
+                activeLane = Random.Range(0, lanes.Length);
+                currentPos = 0;
+                Transform newPos = lanes[activeLane].NextPos(currentPos);
+                forester.transform.position = newPos.position;
+                forester.transform.rotation = newPos.rotation;
+
+                foreach (LaneManager lane in lanes)
+                {
+                    lane.OpenLane();
+                }
+
+                Invoke("MoveForester", 10f);
+            }
+            else
+            {
+                inside = true;
+
+                MoveForward();
+            }
+        }
+    }
+
+    void MoveForward()
+    {
+        currentPos++;
+        Transform newPos = lanes[activeLane].NextPos(currentPos);
+        forester.transform.position = newPos.position;
+        forester.transform.rotation = newPos.rotation;
+    }
+}
