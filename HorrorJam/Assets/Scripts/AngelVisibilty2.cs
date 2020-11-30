a7300fe7a8e789f075c640e26573f326639aa166
jennatark <jennartark@gmail.com>
2020-11-30 14:53:35 -0700

    Revert "Merge branch 'jennaFinal' of https://github.com/Weegeesaurus/Jam3 into jennaFinal"
    This reverts commit c291a7591156a7efe962450d55d1c26c950abadb, reversing
    changes made to dc3504fc731cfa076dec164ffd4590c8a9437704.

---
+++ HorrorJam/Assets/Scripts/AngelVisibilty2.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngelVisibilty2 : MonoBehaviour
+{
+    public Renderer playerVision;
+    public bool visible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerVision = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (playerVision.isVisible)
+        {
+            // print("Hit!");
+            AngelMove2.move = false;
+            AngelMove2.roam = false;
+        }
+        else
+        {
+            //print("Miss!");
+
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, 25f);
+            if (hitColliders.Length > 0)
+            {
+                foreach (var hitCollider in hitColliders)
+                {
+                    if (hitCollider.gameObject.CompareTag("Player"))
+                    {
+                        //print("Hit!");
+                        //transform.position = Vector3.MoveTowards(transform.position, hitCollider.transform.position, .05f);
+                        AngelMove2.move = true;
+                        AngelMove2.roam = false;
+                    }
+                    else
+                    {
+                        AngelMove2.move = false;
+                        AngelMove2.roam = true;
+                    }
+                }
+            }
+            else
+            {
+                AngelMove2.move = false;
+                AngelMove2.roam = true;
+            }
+        }
+
+
+    }
+}
