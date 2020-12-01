686cb4a157ee4f43ad1be0677dad20a56a0fff6e
Nolano-Smurphy <nolan@AZMurphys.com>
2020-11-30 22:54:52 -0700

    Updated IsThisLoss.cs and Created KillCube.cs
    IsThisLoss: updated the comments, resolved an infinite recursion error
    KillCube: initiates a game over condition via a trigger collider

---
+++ HorrorJam/Assets/Scripts/Flickerer/KillCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        //Sets the game in a GAME OVER state if the player is in the room when the lights go out
+        if (col.gameObject.tag == "Player")
+        {
+            //col.gameObject.transform.position = new Vector3(-1000f, -1000f, -1000f);
+            Debug.Log("GAME OVER");
+            WinLoss.gameLose = true;
+        }
+    }
+}
